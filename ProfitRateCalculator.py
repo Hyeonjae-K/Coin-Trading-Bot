e2168ee8fd714466c9c708f56563356ecf74efc2
Hyeonjae-K <rnjsguswo012@hufs.ac.kr>
2022-01-04 11:38:59 +0900

    FEAT - 가중치의 변화에 따른 수익률 출력 기능 추가

--- ProfitRateCalculator.py
+++ ProfitRateCalculator.py
@@ -1,19 +1,27 @@
 import pyupbit
 
 
-def volatilityBreakoutSystems():
+def volatilityBreakoutSystems(df):
     # 변동성 돌파전략 수익률 계산기
-    df = pyupbit.get_ohlcv("KRW-BTC")
     df['range'] = [df['high'][i] - df['low'][i] for i in range(len(df))]
-    profit_rate = 1
+    w = max_profit_rate = max_weight = 0
 
-    for i in range(1, len(df)):
-        target = df['open'][i] + df['range'][i-1] * 0.5
-        if df['high'][i] >= target:
-            profit_rate *= df['close'][i] / target
+    while w < 1:
+        profit_rate = 1
+        w += 0.01
+        for i in range(1, len(df)):
+            target = df['open'][i] + df['range'][i-1] * w
+            if df['high'][i] >= target:
+                profit_rate *= df['close'][i] / target
 
-    print("변동성 돌파전략: %.3f%%, 단순 보유: %.3f%%" %
-          (profit_rate, df['open'][-1]/df['open'][0]))
+        if profit_rate > max_profit_rate:
+            max_profit_rate = profit_rate
+            max_weight = w
+        print("가중치: %.2f, 변동성 돌파전략: %.2f%%" % (w, profit_rate * 100))
+
+    print("단순 보유: %.2f%%" % (df['open'][-1] * 100 / df['open'][0]))
+    print("최대 수익률: %.2f%%, 가중치: %.2f" % (max_profit_rate * 100, max_weight))
 
 
-volatilityBreakoutSystems()
+df = pyupbit.get_ohlcv("KRW-BTC")
+volatilityBreakoutSystems(df)
