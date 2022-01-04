2b9da5d367f9e60951a43c8da2da5897ce66b9d4
Hyeonjae-K <rnjsguswo012@hufs.ac.kr>
2022-01-04 16:55:23 +0900

    FEAT - target 백그라운드 업데이트 기능 추가

--- VolatilityBreakoutSystems.py
+++ VolatilityBreakoutSystems.py
@@ -1,40 +1,60 @@
 import pyupbit
 import time
 import datetime
+from threading import Thread
+from apscheduler.schedulers.background import BackgroundScheduler
 
 import Secret
 
-upbit = pyupbit.Upbit(Secret.access, Secret.secret)
 
+def update_target():
+    global target, is_updated, order_uuid
+    print('update_target')
 
-def get_target():
-    df = pyupbit.get_ohlcv("KRW-BTC", "day")
+    while True:
+        df = pyupbit.get_ohlcv("KRW-BTC", count=11)
+        df_date = str(df.index[-1]).split()[0]
+        today_date = str(datetime.datetime.now()).split()[0]
+        if df_date == today_date:
+            break
+        time.sleep(1)
+    df['range'] = [df['high'][i] - df['low'][i] for i in range(len(df))]
+    upbit.sell_market_order("KRW-BTC", upbit.get_balance("KRW-BTC"))
+    upbit.cancel_order(order_uuid)
+
+    weight = max_weight = max_profit_rate = 0
+    while weight < 1:
+        profit_rate = 1
+        weight += 0.01
+        for i in range(1, len(df)):
+            target = df['open'][i] + df['range'][i-1] * weight
+            if df['high'][i] >= target:
+                profit_rate *= df['close'][i] / target
+        if profit_rate > max_profit_rate:
+            max_profit_rate = profit_rate
+            max_weight = weight
+
+    weight = max_weight
     yesterday = df.iloc[-2]
     today = df.iloc[-1]
-    yesterday_range = yesterday['high'] - yesterday['low']
-    target = today['open'] + yesterday_range * 0.5
-    return target
+    target = today['open'] + yesterday['range'] * weight
+    target = int(target / 1000)
+    target *= 1000
+    is_updated = True
 
 
-target = get_target()
-op_mode = hold = False
+sched = BackgroundScheduler()
+sched.add_job(update_target, 'cron', hour='9')
+sched.start()
+upbit = pyupbit.Upbit(Secret.access, Secret.secret)
+target = is_updated = order_uuid = False
 while True:
-    now = datetime.datetime.now()
-    if now.hour == 9 and now.minute == 1 and now.second < 3:
-        target = get_target()
-        op_mode = True
-        time.sleep(10)
+    if is_updated:
+        now_price = pyupbit.get_current_price("KRW-BTC")
+        if now_price >= target:
+            is_updated = False
+            krw_balance = upbit.get_balance("KRW")
+            bill = upbit.buy_market_order("KRW-BTC", krw_balance)
+            order_uuid = bill['uuid']
 
-    price = pyupbit.get_current_price("KRW-BTC")
-    if op_mode is True and price >= target and hold is False:
-        krw_balance = upbit.get_balance("KRW")
-        upbit.buy_market_order("KRW-BTC", krw_balance)
-        hold = True
-
-    if now.hour == 8 and now.minute == 59 and 50 <= now.second <= 59:
-        if op_mode is True and hold is True:
-            btc_balance = upbit.get_balance("KRW-BTC")
-            upbit.sell_market_order("KRW-BTC", btc_balance)
-            hold = False
-        op_mode = False
-        time.sleep(10)
+    time.sleep(0.5)
